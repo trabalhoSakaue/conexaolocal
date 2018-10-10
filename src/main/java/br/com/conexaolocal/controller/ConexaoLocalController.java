2d4492b4d4ee97565c6a08b16b4125ce925928ce
daf198103 <david.franca01@fatec.sp.gov.br>
2018-10-10 09:11:14 -0300

    modificação porta

--- src/main/java/br/com/conexaolocal/controller/ConexaoLocalController.java
+++ src/main/java/br/com/conexaolocal/controller/ConexaoLocalController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.conexaolocal.entidade.Hello;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin()
 @RequestMapping("/api")
 public class ConexaoLocalController {
 	 
